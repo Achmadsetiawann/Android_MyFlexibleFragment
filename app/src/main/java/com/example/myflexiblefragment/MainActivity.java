b83a01bf8afe5fe5d9205648c32adc3dbe24d043
Achmadsetiawan <achmadsetiawannn@gmail.com>
2019-08-26 23:11:35 +0700

    Add HomeFragment

--- app/src/main/java/com/example/myflexiblefragment/MainActivity.java
+++ app/src/main/java/com/example/myflexiblefragment/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.myflexiblefragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 
 /*
-
-Structure MainActivity =>  activity_main.xml => MoveWithDataActivity.java / Uri.parse("tel:"+phoneNumber)
+                           fragment_home.xml => HomeFragment.java
+Structure MainActivity =>  activity_main.xml
 
 */
 
@@ -16,5 +20,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+
+        Function Panggil Fragment
+        berdasarkan R.id.frame_container
+        pada layout fragment_home.xml
+
+        */
+
+        FragmentManager mFragmentManager = getSupportFragmentManager();
+        FragmentTransaction mFragmentTransaction = mFragmentManager.beginTransaction();
+        HomeFragment mHomeFragment = new HomeFragment();
+
+
+        /* Cek Pesan Erorr pada Function Fragment */
+
+        Fragment fragment = mFragmentManager.findFragmentByTag(HomeFragment.class.getSimpleName());
+        if (!(fragment instanceof HomeFragment)) {
+            mFragmentTransaction.add(R.id.frame_container, mHomeFragment, HomeFragment.class.getSimpleName());
+            Log.d("MyFlexibleFragment", "Fragment Name :" + HomeFragment.class.getSimpleName());
+            mFragmentTransaction.commit();
+        }
+
+
     }
 }
